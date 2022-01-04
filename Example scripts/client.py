95ea997c2d4b08bf1b669a37f30f29539efe5556
Graham Reid <reidg@umd.edu>
2022-01-04 16:54:31 -0500

    Fixed UDP client

--- Example scripts/client.py
+++ Example scripts/client.py
@@ -6,8 +6,9 @@ image_address = ('127.0.0.1', 52986)
 control_address = ('127.0.0.1', 52985)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto("Hello world", control_address)
+message = "Hello World"
+sock.sendto(message.encode(), image_address)
 
 data = sock.recv(2048)
-print ":".join("{:02x}".format(ord(c)) for c in data)
-
+for c in data:
+    print(":".join("{:02x}".format(ord(c))))
